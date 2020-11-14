237b0ac532c0cbfa7d71a19036035368ba787dd3
HyeAnn <60637683+happy1004ann@users.noreply.github.com>
2020-11-14 17:27:18 +0900

    Create fail_04.cpp
    testcase 21/40, memory 12960 kb, 922ms, failed

---
+++ D4/3752/fail_04.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+//#include <cstdio>
+#include <set>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+//	freopen("3752.txt", "r", stdin);
+	cin>>T;
+	for(test_case = 1; test_case <= T; ++test_case)
+	{
+        int problem;
+        cin >> problem;
+
+        int maxpoint = 0;
+        vector<int> points(problem);
+        for (int i=0;i<problem;i++){
+            int point;
+            cin >> point;
+            points[i] = point;
+            maxpoint += point;
+        }
+        sort(points.begin(), points.end());
+
+        set<int> score {0};
+        int size;
+        bool brk = false;
+
+        for (int i=0;i<problem;i++){
+            set<int> temp(score);
+            set<int>::iterator iter = temp.begin();
+            for (;iter != temp.end();iter++){
+                int cal = *iter + points[i];
+                if (2 * cal < maxpoint){
+                    score.insert(cal);
+                    continue;
+                }
+                brk = true;
+                size = 2 * score.size();
+                if (2 * cal == maxpoint) size++;
+                break;
+            }
+            if (brk) break;
+        }
+        cout << "#" << test_case << " ";
+        cout << size << endl;
+	}
+	return 0;
+}
