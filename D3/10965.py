18dc709732d81c74f898477082acff5f934b983b
HyeAnn <happy1004ann@gmail.com>
2024-03-07 14:11:05 +0900

    Fail: timeout

--- D3/10965.py
+++ D3/10965.py
@@ -36,13 +36,16 @@ print(f)                                문자열 1개 출력하는 예제
 #import sys
 #sys.stdin = open("input.txt", "r")
 
+import math
+
 T = int(input())
+answer = []
+
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
-    # ///////////////////////////////////////////////////////////////////////////////////
-    '''
-
-        이 부분에 여러분의 알고리즘 구현이 들어갑니다.
-
-    '''
-    # ///////////////////////////////////////////////////////////////////////////////////
+    A = int(input())
+    for i in range(int(math.sqrt(A)), 1, -1):
+        if A%(i**2)==0:
+            A = A//(i**2)
+    answer.append(f'#{test_case} {A}')
+print('\n'.join(answer))
