2237ba16dc5628ca9b9c3d226e9ba5512a0d951c
HyeAnn <happy1004ann@gmail.com>
2024-03-13 20:54:32 +0900

    Q11445 Fail: wrong answer
    Pass 76 out of 100
    3rd submission

--- D3/11445 Infinite dictionary.py
+++ D3/11445 Infinite dictionary.py
@@ -44,12 +44,6 @@ for test_case in range(1, T + 1):
 
     if P+'a'==Q:  # man vs mana
         result = 'N'
-    
-    elif P[:-1]==Q[:-1] and ord(P[-1])+1==ord(Q[-1]):  # mana vs manb
-        result = 'N'
-    
-    elif P[1:]=='z'*(len(P)-1) and len(Q)==1 and ord(P[0])+1==ord(Q): # az...z vs b
-        result = 'N'
 
     else:
         result = 'Y'
