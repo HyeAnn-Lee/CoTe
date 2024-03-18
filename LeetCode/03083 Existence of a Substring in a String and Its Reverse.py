c49dd316a149a58b300f01045264f825b3f70eb0
HyeAnn <happy1004ann@gmail.com>
2024-03-18 15:05:08 +0900

    Accepted
    713 / 713 test cases passed.
    *
    Runtime: 26 ms
    Memory Usage: 16.5 MB

--- LeetCode/03083 Existence of a Substring in a String and Its Reverse.py
+++ LeetCode/03083 Existence of a Substring in a String and Its Reverse.py
@@ -1,3 +1,7 @@
 class Solution:
     def isSubstringPresent(self, s: str) -> bool:
-        
+        rev = ''.join(list(s)[::-1])
+        for i in range(len(s)-1):
+            if s[i:i+2] in rev:
+                return True
+        return False
