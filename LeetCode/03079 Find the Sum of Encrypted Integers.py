eaee0c9478ccb9f56a60c2145e4f981fdf2a3173
HyeAnn <happy1004ann@gmail.com>
2024-03-18 14:43:11 +0900

    Wrong answer
    118 / 865 test cases passed.
    *
    Input: [10,21,31]
    Output: 12
    Expected: 66

--- LeetCode/03079 Find the Sum of Encrypted Integers.py
+++ LeetCode/03079 Find the Sum of Encrypted Integers.py
@@ -1,2 +1,11 @@
+from typing import List
+
+
 class Solution:
     def sumOfEncryptedInt(self, nums: List[int]) -> int:
+        ans = 0
+        for num in nums:
+            num_str = str(num) 
+            encrypt = int(max(num_str))*len(num_str)
+            ans += encrypt
+        return ans
