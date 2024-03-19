e51264fd728c5ecf7a7c980e3866040d57d8d8d5
HyeAnn <happy1004ann@gmail.com>
2024-03-19 19:56:57 +0900

    Add another approach

--- Baekjoon/01932 Integer triangle.py
+++ Baekjoon/01932 Integer triangle.py
@@ -6,7 +6,10 @@ n = int(input())
 
 triangle = [list(map(int, input().split())) for _ in range(n)]
 
+### Bottom-up
+
 for i in range(n-1, 0, -1):
+    
     subsum = [None] * i
     for j in range(i):
         subsum[j] = triangle[i-1][j] + max(triangle[i][j:j+2])
@@ -14,3 +17,15 @@ for i in range(n-1, 0, -1):
     triangle[i-1] = subsum
 
 print(triangle[0][0])
+
+
+### Top-down
+
+for i in range(1, n):
+
+    triangle[i][0] += triangle[i-1][0]
+    for j in range(1, i):
+        triangle[i][j] += max(triangle[i-1][j], triangle[i-1][j-1])
+    triangle[i][i] += triangle[i-1][i-1]
+
+print(max(triangle[-1]))
