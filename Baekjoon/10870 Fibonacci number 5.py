e7462e335dd68046b8b226424fd73f5dcfefc978
HyeAnn <happy1004ann@gmail.com>
2024-03-19 15:47:12 +0900

    Pass
    31120kB, 40ms

--- Baekjoon/10870 Fibonacci number 5.py
+++ Baekjoon/10870 Fibonacci number 5.py
@@ -1,3 +1,14 @@
 '''
 피보나치 수 역시 단순 for문으로도 구할 수 있지만, 학습을 위해 재귀를 써 봅시다.
 '''
+
+def fibbo(x):
+    if x==0:
+        return 0
+    if x==1:
+        return 1
+    return fibbo(x-1)+fibbo(x-2)
+
+n = int(input())
+
+print(fibbo(n))
