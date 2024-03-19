058f5e4df9269f9081ab9ae90c73b063064d9545
HyeAnn <happy1004ann@gmail.com>
2024-03-19 17:58:12 +0900

    Pass
    35612kB, 152ms

--- Baekjoon/01932 Integer triangle.py
+++ Baekjoon/01932 Integer triangle.py
@@ -1,3 +1,18 @@
 '''
 각 층의 모든 칸마다 최댓값을 저장하면서 동적 계획법으로 푸는 문제
 '''
+
+n = int(input())
+
+triangle = []
+for _ in range(n):
+    triangle.append(list(map(int, input().split())))
+
+for i in range(n-1, 0, -1):
+    subsum = [None] * i
+    for j in range(i):
+        subsum[j] = triangle[i-1][j] + max(triangle[i][j:j+2])
+
+    triangle[i-1] = subsum
+
+print(triangle[0][0])
