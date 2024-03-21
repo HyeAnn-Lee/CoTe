8f6ff603609bee2d5fd792dfb38f4fc4cbcf32a5
HyeAnn <happy1004ann@gmail.com>
2024-03-21 22:23:16 +0900

    Fail - Timeout
    Remove redundant subsum operation
    
    백준 11066번, 파일 합치기

--- Baekjoon/11066 Merging files.py
+++ Baekjoon/11066 Merging files.py
@@ -9,11 +9,15 @@ for _ in range(T):
 
     cost = [[0]*K for _ in range(K)]
 
+    # File size
     for i in range(K-1):
         cost[i][i+1] = files[i] + files[i+1]
+        for j in range(i+2, K):
+            cost[i][j] = cost[i][j-1] + files[j]
 
+    # Merging cost
     for i in range(2, K):
         for j in range(K-i):
-            cost[j][i+j] = min([cost[j][k] + cost[k+1][i+j] for k in range(j, i+j)]) + sum(files[j:i+j+1])
+            cost[j][i+j] += min([cost[j][k] + cost[k+1][i+j] for k in range(j, i+j)])
 
     print(cost[0][-1])
