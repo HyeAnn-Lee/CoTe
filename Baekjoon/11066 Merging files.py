2414da4b354cbd8780ac56251949380a9c0e7450
HyeAnn <happy1004ann@gmail.com>
2024-03-21 21:40:51 +0900

    Fail - Timeout
    백준 11066번, 파일 합치기

--- Baekjoon/11066 Merging files.py
+++ Baekjoon/11066 Merging files.py
@@ -1,3 +1,19 @@
 """
 파일을 합쳐 하나로 모으는 최소 비용을 구하는 문제
 """
+
+T = int(input())
+for _ in range(T):
+    K = int(input())
+    files = list(map(int, input().split()))
+
+    cost = [[0]*K for _ in range(K)]
+
+    for i in range(K-1):
+        for j in range(K-i-1):
+            cost_list = []
+            for k in range(j, i+j+1):
+                cost_list.append(cost[j][k]+cost[k+1][i+j+1])
+            cost[j][i+j+1] = min(cost_list) + sum(files[j:i+j+2])
+
+    print(cost[0][-1])
