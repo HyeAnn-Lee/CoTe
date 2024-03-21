a6d077e2996c602648b90387125c540a39366b32
HyeAnn <happy1004ann@gmail.com>
2024-03-21 19:59:41 +0900

    Fail - Timeout
    백준 24416번, 알고리즘 수업 - 피보나치 수 1

--- Baekjoon/24416 Algorithm class - Fibonacci number 1.py
+++ Baekjoon/24416 Algorithm class - Fibonacci number 1.py
@@ -1,27 +1,23 @@
 n = int(input())
 max_n = 40
-fibo_list = [0] * (max_n+1)
 
-count_recur = 0
-count_dp = 0
 
 def fibo_recur(n):
-    global count_recur
-
     if n==1 or n==2:
-        count_recur += 1
         return 1
     return fibo_recur(n-1) + fibo_recur(n-2)
 
 def fibo_dp(n):
-    global count_dp
+    count_dp = 0
     
+    fibo_list = [0] * (max_n+1)
     fibo_list[1] = fibo_list[2] = 1
+    
     for i in range(3, n+1):
         count_dp += 1
         fibo_list[i] = fibo_list[i-1] + fibo_list[i-2]
-    return fibo_list[n]
+    return fibo_list[n], count_dp
 
-fibo_recur(n)
-fibo_dp(n)
+count_recur = fibo_recur(n)
+_, count_dp = fibo_dp(n)
 print(count_recur, count_dp)
