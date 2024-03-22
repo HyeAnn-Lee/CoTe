b0dea4366d853e945c8a105d44020726f72b2176
HyeAnn <happy1004ann@gmail.com>
2024-03-22 14:30:56 +0900

    Fail - Timeout
    백준 2805번, 나무 자르기 (실버2)

--- Baekjoon/02805 Tree cutting.py
+++ Baekjoon/02805 Tree cutting.py
@@ -6,10 +6,15 @@
 5 5
 will output: 3
 
-<TC2>
+<TC2>   # cannot get exactly 1. should get 5 trees.
 5 1
 2 2 2 2 2
 will output: 1
+
+<TC3>   # should cut all trees.
+5 10
+2 2 2 2 2
+will output: 0
 
 """
 
@@ -17,7 +22,7 @@ N, M = list(map(int, input().split()))
 trees = list(map(int, input().split()))
 
 max_M = max(trees)
-min_M = 1
+min_M = 0
 
 while True:
     H = (max_M+min_M)//2
@@ -27,12 +32,12 @@ while True:
     
     if get == M:    # found!
         break
-    elif get > M:     # too many trees. increase H.
+    elif get > M:   # too many trees. increase H.
         min_M = H
     else:           # need more trees. decrease H.
         max_M = H
 
-    if max_M - min_M <= 1:
+    if max_M-min_M == 1 and get>M:
         break
 
 print(H)
