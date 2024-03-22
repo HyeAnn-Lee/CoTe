e235b30f20b21e4e167f2a8c9c23ae52781f0806
HyeAnn <happy1004ann@gmail.com>
2024-03-22 16:28:11 +0900

    Pass
    백준 2178번, 미로 탐색 (실버1)
    34668kB, 96ms

--- Baekjoon/02178 Maze search.py
+++ Baekjoon/02178 Maze search.py
@@ -9,28 +9,28 @@ E = []
 for i in range(N):
     for j in range(M):
         ind = i*M + j
-        if map[ind] == 0:
-            continue
+        edges = []
 
-        add = []
-        if i!=0 and map[ind-M]==1:
-            add.append((ind, ind-M))
-        if i!=N-1 and map[ind+M]==1:
-            add.append((ind, ind+M))
-        if j!=0 and map[ind-1]==1:
-            add.append((ind, ind-1))
-        if j!=M-1 and map[ind+1]==1:
-            add.append((ind, ind+1))
-        E.extend(add)
+        if map[ind] != 0:
+            if i!=0 and map[ind-M]==1:
+                edges.append(ind-M)
+            if i!=N-1 and map[ind+M]==1:
+                edges.append(ind+M)
+            if j!=0 and map[ind-1]==1:
+                edges.append(ind-1)
+            if j!=M-1 and map[ind+1]==1:
+                edges.append(ind+1)
+
+        E.append(edges)
 
 dist = [float("inf")] * (N*M)
 dist[0] = 0
 queue = deque([0])
 while len(queue) != 0:
     u = queue.popleft()
-    for v1, v2 in E:
-        if v1 == u and dist[v2] == float("inf"):
+    for v2 in E[u]:
+        if dist[v2] == float("inf"):
             queue.append(v2)
-            dist[v2] = dist[v1] + 1
+            dist[v2] = dist[u] + 1
 
 print(dist[-1]+1)
