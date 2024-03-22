53ce549eb5c09cdd5ba47cb341f1af6768b47722
HyeAnn <happy1004ann@gmail.com>
2024-03-22 13:58:00 +0900

    Fail - Timeout
    백준 2805번, 나무 자르기 (실버2)

--- Baekjoon/02805 Tree cutting.py
+++ Baekjoon/02805 Tree cutting.py
@@ -1,5 +1,16 @@
 """
 이분 탐색을 응용하여 최솟값이나 최댓값을 찾는 문제 2
+
+<TC1>
+2 4
+5 5
+will output: 3
+
+<TC2>
+5 1
+2 2 2 2 2
+will output: 1
+
 """
 
 N, M = list(map(int, input().split()))
@@ -8,15 +19,18 @@ trees = list(map(int, input().split()))
 max_M = max(trees)
 min_M = 1
 
-while (max_M - min_M != 1):
+while True:
     H = (max_M+min_M)//2
     get = 0
     for tree in trees:
         get += max(0, tree-H)
     
-    if get > M:     # too many trees. increase H.
+    if get >= M:     # too many trees. increase H.
         min_M = H
     else:           # need more trees. decrease H.
         max_M = H
 
-print(max_M)
+    if max_M - min_M <= 1:
+        break
+
+print(min_M)
