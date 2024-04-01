86231b3a44bf3504d0f6a01988a5819e8a0ebf31
HyeAnn <happy1004ann@gmail.com>
2024-04-01 22:51:09 +0900

    Pass
    프로그래머스, 전력망을 둘로 나누기 (Lv 2)
    완전탐색

---
+++ Programmers/Splitting the electrical grid in half.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+
+def solution(n, wires):
+    # 각 edge에 가상의 node를 추가하고 그 node를 root로 만들었을 때, 두 subtree의 크기를 비교
+    
+    answer = n
+    
+    for i in range(n-1):
+        
+        subroot1, _ = wires[i]
+        wires_test = wires[:i] + wires[i+1:]
+        
+        E = [[] for _ in range(n+1)]
+        for v1, v2 in wires_test:
+            E[v1].append(v2)
+            E[v2].append(v1)
+
+        subsize1 = 1
+        
+        visited = [False] * (n+1)
+        Q = deque([subroot1])
+        while len(Q) != 0:
+            u = Q.popleft()
+            visited[u] = True
+            for neigh in E[u]:
+                if visited[neigh] == False:
+                    Q.append(neigh)
+                    subsize1 += 1
+            
+        diff = abs(subsize1 - (n-subsize1))
+        answer = min(diff, answer)
+
+    return answer
