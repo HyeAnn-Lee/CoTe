6da7feab031d2eeffe1a339d0f9beb164a7dbbf3
HyeAnn <happy1004ann@gmail.com>
2024-04-22 11:57:31 +0900

    Accepted 33ms / 16.57MB
    Leetcode Weekly Contest 394
    Difficulty: Easy

--- LeetCode/03120 Count the Number of Special Characters I.py
+++ LeetCode/03120 Count the Number of Special Characters I.py
@@ -1,3 +1,19 @@
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
-        pass
+        
+        lowerbag, upperbag = set(), set()
+        diff = ord('a') - ord('A')
+        count = 0
+        
+        for letter in word:
+            temp = ord(letter)
+            if temp >= ord('a'):
+                lowerbag.add(temp)
+            else:
+                upperbag.add(temp)
+        
+        for upper in upperbag:
+            if upper + diff in lowerbag:
+                count += 1
+            
+        return count
